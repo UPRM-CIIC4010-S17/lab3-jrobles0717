eab8ec81781e61865012f52bc8e4b55ad5c3adbe
j_rob <j_rob@LAPTOP-C5RO7NGG.lan>
2017-11-25 18:43:30 -0500

    a~adi un borde extra

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+		JFrame myFrame = new JFrame("John Robles");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		myFrame.setLocation(1700, 900);
+		myFrame.setSize(500, 500);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
